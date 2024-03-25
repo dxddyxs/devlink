499ec3f34376ad35a49e354d832f863a2cb9aa7d
dxddyxs <derecklisboaj3@gmail.com>
2024-03-25 16:00:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // pegar a tag img
+    const img = document.querySelector('#profile img')
+
+    // substituir a imagem
+    if(html.classList.contains('light')) {
+        // se tiver light mode, add img light
+        img.setAttribute('src', './background/avatar-light.png')
+    } else {
+        // se tiver sem light, manter img
+        img.setAttribute('src', './background/avatar.png')
+    }
+
+
+}
